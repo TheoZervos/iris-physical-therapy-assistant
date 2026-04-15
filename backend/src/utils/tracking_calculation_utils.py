efc1b1549f8c03935dfdc0f5435e14d1787a90e1
TheoZervos <112411316+TheoZervos@users.noreply.github.com>
2026-04-14 23:56:06 -0400

    Updated tracking_calculation.py

--- backend/src/utils/tracking_calculation_utils.py
+++ backend/src/utils/tracking_calculation_utils.py
@@ -3,16 +3,24 @@ from src.schemas.pose_schema import JOINT_MAP, PoseFrame
 
 def calculate_angle(pose_frame: PoseFrame, joint: str) -> float | None:    
     # Getting the landmarks for requested joint
+    # a - landmark1, b - landmark2, c - landmark3
     landmark1, landmark2, landmark3 = JOINT_MAP.get(joint)
-    if not all(0 <= lm < len(pose_frame.landmarks) for lm in [landmark1, landmark2, landmark3]):
+    opposite_joint = joint.replace("left", "right") if "left" in joint else joint.replace("right", "left")
+    alt_lm1, alt_lm2, alt_lm3 = JOINT_MAP.get(opposite_joint)
+    
+    missed_landmarks = []
+    for index, lm in enumerate([landmark1, landmark2, landmark3]):
+        if pose_frame.landmarks[lm].visibility < 0.4:
+            missed_landmarks.append((index, lm))
+            
+    if len(missed_landmarks) > 2:
         return None
-
-    a = (pose_frame.landmarks[landmark1].x, pose_frame.landmarks[landmark1].y)
-    b = (pose_frame.landmarks[landmark2].x, pose_frame.landmarks[landmark2].y)
-    c = (pose_frame.landmarks[landmark3].x, pose_frame.landmarks[landmark3].y)
-
-    ba = [a[i] - b[i] for i in range(len(a))]
-    bc = [c[i] - b[i] for i in range(len(c))]
+    
+    opposite_joint = joint.replace("left", "right") if "left" in joint else joint.replace("right", "left")
+    a = landmark1 if landmark1 not in missed_landmarks else 
+    
+    ba = [landmark1[i] - landmark2[i] for i in range(len(landmark1))]
+    bc = [landmark3[i] - landmark2[i] for i in range(len(landmark3))]
 
     dotProduct = sum(ba[i] * bc[i] for i in range(len(ba)))
     mag_ba = math.sqrt(sum(x * x for x in ba))
